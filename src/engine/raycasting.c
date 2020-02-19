e30df71f07fea86e15e88d987cee0848b4b0a078
mrouabeh <mrouabeh@student.42.fr>
2020-02-19 10:19:47 +0100

    Petites corrections

--- src/engine/raycasting.c
+++ src/engine/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <mrouabeh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 13:36:08 by mrouabeh          #+#    #+#             */
-/*   Updated: 2020/02/18 13:54:46 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2020/02/19 10:05:33 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,29 @@ void	raycasting(
 	x = 0;
 	while (x < window->width)
 	{
-		ray.camera_x = (2 * x) / window->width - 1;
+		ray.camera_x = (2 * x) / (float)window->width - 1;
 		ray.ray_x = player->dir_x + player->plan_x * ray.camera_x;
 		ray.ray_y = player->dir_y + player->plan_y * ray.camera_x;
+
 		dda(game->layout, player, &ray);
+
 		line_height = (int)(window->height / ray.perp_wall_dist);
-		printf("\nperp_wall_dist: %f\n", ray.perp_wall_dist);
-		printf("line_height: %d\n", line_height);
+
+		printf("x: %d\n", x);
+		printf("camera_x: %f\n", ray.camera_x);
+		printf("perp_wall_dist: %f\n", ray.perp_wall_dist);
+		printf("ray_x: %f\n", ray.ray_x);
+		printf("ray_y: %f\n", ray.ray_y);
+		printf("side_dist_x: %f\n", ray.side_dist_x);
+		printf("side_dist_y: %f\n", ray.side_dist_y);
+		printf("delta_dist_x: %f\n", ray.delta_dist_x);
+		printf("delta_dist_y: %f\n", ray.delta_dist_y);
+		printf("step_x: %d\n", ray.step_x);
+		printf("step_y: %d\n", ray.step_y);
+		printf("side: %d\n", ray.side);
+		printf("hit: %d\n", ray.hit);
+		printf("line_height: %d\n\n\n", line_height);
+
 		draw_col(game, window, line_height, x);
 		x++;
 	}
