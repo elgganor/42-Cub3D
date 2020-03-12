1bf8931243156019b66ad94654d1efaae64450b2
mrouabeh <mrouabeh@student.42.fr>
2020-03-12 12:59:51 +0100

    Partie screenshot

--- src/render/bitmap.c
+++ src/render/bitmap.c
@@ -6,21 +6,11 @@
 /*   By: mrouabeh <mrouabeh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 08:31:09 by mrouabeh          #+#    #+#             */
-/*   Updated: 2020/03/12 10:40:17 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2020/03/12 12:55:42 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static int	get_pixel(t_image *image, int x, int y)
-{
-	int rgb;
-	int color;
-
-	color = image->img_data[y * image->size_line + x *image->bpp];
-	rgb = (color & 0xFF0000) | (color & 0x00FF00) | (color & 0x0000ff);
-	return (rgb);
-}
 
 static void	file_header(t_image *image, int fd)
 {
@@ -45,8 +35,8 @@ static void	image_header(t_game *game, t_image *image, int fd)
 	plane = 1;
 	image_size = game->window->width * game->window->height;
 	write(fd, &header_size, 4);
-	write(fd, &image->width, 4);
-	write(fd, &image->height, 4);
+	write(fd, &game->window->width, 4);
+	write(fd, &game->window->height, 4);
 	write(fd, &plane, 2);
 	write(fd, &image->bpp, 2);
 	write(fd, "\0\0\0\0", 4);
@@ -54,23 +44,30 @@ static void	image_header(t_game *game, t_image *image, int fd)
 	write(fd, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 16);
 }
 
-static void	fill_bitmap(t_image *image, int fd)
+static void	fill_bitmap(t_game *game, t_image *image, int fd)
 {
 	int	x;
 	int y;
-	int	rgb;
+	int	r;
+	int	g;
+	int	b;
 
-	y = 0;
-	while (y < image->height)
+	y = game->window->height - 1;
+	while (y >= 0)
 	{
 		x = 0;
-		while (x < image->width)
+		while (x < game->window->width)
 		{
-			rgb = get_pixel(image, x, y);
-			write(fd, &rgb, 3);
+			r = image->img_data[y * image->size_line + x * image->bpp / 8];
+			g = image->img_data[y * image->size_line + x * image->bpp / 8 + 1];
+			b = image->img_data[y * image->size_line + x * image->bpp / 8 + 2];
+			write(fd, &r, 1);
+			write(fd, &g, 1);
+			write(fd, &b, 1);
+			write(fd, "\0", 1);
 			x++;
 		}
-		y++;
+		y--;
 	}
 }
 
@@ -78,13 +75,13 @@ void	create_bitmap(t_game *game)
 {
 	int	fd;
 
-	if ((fd = open("Cub3d.bmp", O_CREAT | O_RDWR)) < 0)
+	if ((fd = open("screenshot.bmp", O_CREAT | O_RDWR)) < 0)
 	{
 		clear_game(game);
 		exit_failure("Impossible to create the bitmap file;\n");
 	}
 	file_header(game->image, fd);
 	image_header(game, game->image, fd);
-	fill_bitmap(game->image, fd);
+	fill_bitmap(game, game->image, fd);
 	close(fd);
 }
